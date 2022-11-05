42a258a14af1e2775e9f44939207cbe049ad917b
andreydk <0765871@gmail.com>
2022-11-05 23:27:01 +0600

    init 2

--- src/main/java/com/example/mychat/MyChatApplication.java
+++ src/main/java/com/example/mychat/MyChatApplication.java
@@ -1,18 +1,20 @@
 package com.example.mychat;
 
 import javafx.application.Application;
+import javafx.css.converter.PaintConverter;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.paint.Paint;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class MyChatApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 420);
-        stage.setTitle("MyChat v.0");
+        FXMLLoader fxmlLoader = new FXMLLoader(MyChatApplication.class.getResource("hello-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 300, 400);
+        stage.setTitle("MyChat v.0.1");
         stage.setScene(scene);
         stage.show();
     }
--- src/main/java/com/example/mychat/MyChatController.java
+++ src/main/java/com/example/mychat/MyChatController.java
@@ -1,8 +1,19 @@
 package com.example.mychat;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 
-public class HelloController {
+public class MyChatController {
+    @FXML
+    TextArea chatArea;
+    @FXML
+    TextField inputArea;
+    public void sendMessage (ActionEvent actionEvent){
+        chatArea.appendText(inputArea.getText() + "\n");
+        inputArea.clear();
+
+    }
 
 }
